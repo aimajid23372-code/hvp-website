5f45302badf56f49b9e5056e1aea3c2413db5235
aimajid23372-code <aimajid23372@gmail.com>
2026-09-23 11:58:23 +0600

    Upgrade HVB admin control center and unify course loading

--- api/track.js
+++ api/track.js
@@ -28,7 +28,7 @@ module.exports = async (req, res) => {
       return res.status(200).json({ products: data || [] });
     }
     if (event === 'settings_get') {
-      const allow = ['home_slides','home_hero_title','home_hero_subtitle','home_cta_label','home_hero_image','home_show_courses','home_show_reviews','home_show_faq','announcement','announcement_enabled','site_name','tagline','logo_url','favicon_url','messenger_link','facebook_link','tiktok_link','support_email'];
+      const allow = ['home_slides','home_hero_title','home_hero_subtitle','home_cta_label','home_hero_image','home_show_courses','home_show_reviews','home_show_faq','announcement','announcement_enabled','site_name','tagline','logo_url','favicon_url','messenger_link','facebook_link','tiktok_link','support_email','theme_template','color_primary','color_secondary','color_background','color_surface','course_heading_label','course_cta_label','course_trust_text','course_delivery_note','course_show_description','course_show_reviews','course_show_related','footer_text','support_label','faq_heading','refund_summary','seo_home_title','seo_home_description'];
       const { data } = await supabase.from('settings').select('key, value');
       const out = {};
       (data || []).forEach((s2) => { if (allow.includes(s2.key)) out[s2.key] = s2.value; });
